f83fb68665266a794aba16564a0c4c9ef80570a5
Sasi2294 <montella.salvatore@live.it>
2023-11-22 16:44:09 +0100

    Base work finished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
-// Questo è il file che contiene tutto il codice JS della pagina web
+//Creare un array composto da 10 automobili. Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, elettrica, ibrida, gpl).
+//Dividi le automobili in 3 array separati: un array solo benzina, un array solo diesel e nel terzo tutte le altre.
+//Stampare i tre array separatamente
 
-document.writeln("<h3>Ciao Mondo!</h3>");
 
-alert("Hello Word!");
+let autoList = [
+    { marca: "BMW", modello: "Serie 3", alimentazione: "Diesel"},
+    { marca: "Fiat", modello: "Panda", alimentazione: "Benzina"},
+    { marca: "Tesla", modello: "Model X", alimentazione: "Elettrica"},
+    { marca: "Citroen", modello: "C3", alimentazione: "Diesel"},
+    { marca: "Lamborghini", modello: "Urus", alimentazione: "Benzina"},
+    { marca: "Chevrolet", modello: "Camaro SS", alimentazione: "Benzina"},
+    { marca: "Ferrari", modello: "Roma", alimentazione: "Benzina"},
+    { marca: "Renault", modello: "208", alimentazione: "Ibrida"},
+    { marca: "Ford", modello: "Fiesta", alimentazione: "Diesel"},
+    { marca: "Jaguar", modello: "F-PACE", alimentazione: "Elettrica"}
+]
 
-document.getElementById("title_page").innerHTML = "Ciao Mondo!";
+let autoBenzina = [];
+let autoDiesel = [];
+let autoRemain = [];
 
-console.log("Ciao Mondo!");
+autoList.forEach (function(element) {
+    if (element.alimentazione == "Benzina"){
+        autoBenzina.push(element);
+    } else if (element.alimentazione == "Diesel"){
+        autoDiesel.push(element);
+    } else {
+        autoRemain.push(element);
+    }
+});
+
+console.log (autoBenzina);
+console.log (autoDiesel);
+console.log (autoRemain);
+
+
+
+
+//-------------FUNZIONI------------
